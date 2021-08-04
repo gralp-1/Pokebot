4f8a0cf0ad13368f18acb0928499371016bf8dbe
gralp <62028969+gralp-1@users.noreply.github.com>
2021-08-04 20:19:54 +0100

    Made it an embed

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ client_secret = config("client_secret")
 message_counter = 1
 message_target = 5
 
+client = discord.Client()
 
 class main_bot(discord.Client):
     async def on_ready(self):
@@ -16,19 +17,22 @@ class main_bot(discord.Client):
 
     async def on_message(self, ctx):
         global message_counter, message_target
-        print(message_counter)
-        print(ctx.author)
-        print(self.user)
         if str(ctx.author) != str(self.user):
             if message_counter == message_target:
                 img = json.loads(
                     r.get("https://pokeapi.co/api/v2/pokemon/" + str(rand.randint(1, 898))).text)
-                await ctx.channel.send(img["sprites"]["front_default"])
+
+                embed = discord.Embed(title="PokéBot", colour=discord.Colour.blurple())
+                embed.set_image(url=str(img["sprites"]["front_default"]))
+                embed.set_footer(text=img["name"])
+                
+                await ctx.channel.send(embed=embed)
                 message_counter = 0
                 message_target = rand.randint(50, 300)
                 print("new message target "+str(message_target))
             else:
                 message_counter += 1
+
 
 
 client = main_bot()
